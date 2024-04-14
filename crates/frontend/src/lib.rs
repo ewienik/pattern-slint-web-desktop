8bc11fc652c5abc8221699cc524cecb498c11c77
Pawel Pery <ewienik@post.pl>
2024-04-14 15:11:03 +0200

    Move creating runtime from backend into frontend

--- crates/frontend/src/lib.rs
+++ crates/frontend/src/lib.rs
@@ -18,18 +18,52 @@ mod implementation {
 
 #[cfg(not(target_arch = "wasm32"))]
 mod implementation {
-    use {backend::Backend, defs::Counter};
+    use {
+        backend::Backend,
+        defs::Counter,
+        proto::Message,
+        tokio::{
+            runtime::Runtime,
+            sync::{mpsc, oneshot},
+        },
+    };
+
+    mod proto {
+        use {defs::Counter, tokio::sync::oneshot};
+
+        #[derive(Debug)]
+        pub(super) enum Message {
+            Counter(Counter, oneshot::Sender<Counter>),
+        }
+    }
 
     pub struct Frontend {
-        be: Backend,
+        tx: mpsc::UnboundedSender<Message>,
+        #[allow(dead_code)]
+        rt: Runtime,
     }
 
     impl Frontend {
         pub fn new() -> Self {
-            Self { be: Backend::new() }
+            let rt = Runtime::new().unwrap();
+            let (tx, mut rx) = mpsc::unbounded_channel();
+            rt.spawn(async move {
+                let be = Backend::new();
+                while let Some(msg) = rx.recv().await {
+                    match msg {
+                        Message::Counter(counter, tx) => {
+                            tx.send(be.process_counter(counter).await).unwrap();
+                        }
+                    }
+                }
+            });
+            Self { tx, rt }
         }
-        pub fn process_counter(&self, value: Counter, cont: impl FnMut(Counter)) {
-            self.be.process_counter(value, cont);
+
+        pub fn process_counter(&self, value: Counter, mut cont: impl FnMut(Counter)) {
+            let (tx, rx) = oneshot::channel();
+            self.tx.send(Message::Counter(value, tx)).unwrap();
+            cont(rx.blocking_recv().unwrap());
         }
     }
 }
